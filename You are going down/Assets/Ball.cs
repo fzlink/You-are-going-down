05b6056ca4e1c09141e007b18796c3a348a40786
fzlink <fazlibozatay@gmail.com>
2020-01-28 15:51:27 +0300

    Initial Commit

---
+++ You are going down/Assets/Ball.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+
+    private bool canMove = true;
+    private bool canTurnSwitcher = true;
+    private Vector3 offset;
+    // Start is called before the first frame update
+    void Start()
+    {
+        canMove = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(canMove)
+            transform.position = new Vector3(transform.position.x + 0.1f, transform.position.y, transform.position.z);
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.CompareTag("ClickCollider"))
+            return;
+        if (canTurnSwitcher)
+        {
+            canMove = false;
+            canTurnSwitcher = false;
+            other.gameObject.transform.parent.gameObject.SendMessage("SetIsBallHere", true);
+            
+            offset = transform.position - other.GetComponent<Renderer>().bounds.center;
+        }
+        if(other.gameObject.transform.parent.gameObject.GetComponent<Switcher>().canTurn)
+            FollowSwithcer(other);
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("ClickCollider"))
+            return;
+        other.gameObject.transform.parent.gameObject.SendMessage("SetIsBallHere", false);
+    }
+
+    private void FollowSwithcer(Collider other)
+    {
+        transform.localPosition = other.GetComponent<Renderer>().bounds.center + offset;
+    }
+}
---
+++ You are going down/Assets/Switcher.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Switcher : MonoBehaviour
+{
+
+    private float initRotationX;
+    private float step;
+    public bool canTurn;
+    private bool isBallHere;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(canTurn)
+            Turn();
+    }
+
+    public void Turn()
+    {
+        if (transform.localEulerAngles.x * 2f >= 180 - Mathf.Epsilon)
+        {
+            canTurn = false;
+            transform.rotation = Quaternion.identity;
+            return;
+        }
+        step =  200 * Time.deltaTime;
+        transform.Rotate(Vector3.right, step);
+        print("turn");
+
+    }
+
+    public void SetIsBallHere(bool isBallHere)
+    {
+        this.isBallHere = isBallHere;
+    }
+    public bool GetIsBallHere()
+    {
+        return isBallHere;
+    }
+
+    private void OnMouseDown()
+    {
+        canTurn = true;
+
+    }
+
+}
